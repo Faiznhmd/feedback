655c298b2b41b441d5444c2effb6f06077b2bffe
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-29 16:00:48 +0530

    feedback data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import Header from './component/Header';
+import FeedBackList from './component/FeedBackList';
+import data from './data/FeedbackData';
 import './App.css';
 
 function App() {
+  const [feedback, setFeedback] = useState(data);
+
   return (
     <>
       <Header text={'Faizan App'} />
+      <div className="container">
+        <FeedBackList feedback={feedback} />
+      </div>
     </>
   );
 }
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis atque porro nesciunt temporibus! Sequi impedit eaque, nulla fuga ducimus at',
+  },
+  {
+    id: 2,
+    rating: 1,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis atque porro nesciunt temporibus! Sequi impedit eaque, nulla fuga ducimus at',
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis atque porro nesciunt temporibus! Sequi impedit eaque, nulla fuga ducimus at',
+  },
+  {
+    id: 4,
+    rating: 5,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis atque porro nesciunt temporibus! Sequi impedit eaque, nulla fuga ducimus at',
+  },
+];
+
+export default data;
