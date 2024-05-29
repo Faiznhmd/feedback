8484bdb0b5bc7ca7e6cbe153605d2efebcdafe42
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-29 16:20:35 +0530

    card component

--- src/component/FeedBackItem.jsx
+++ src/component/FeedBackItem.jsx
@@ -1,9 +1,11 @@
+import Card from './Shared/Card';
+
 const FeedBackItem = ({ item }) => {
   return (
-    <div className="card">
+    <Card reverse={true}>
       <div className="num-display">{item.rating} </div>
       <div className="text-display">{item.text} </div>
-    </div>
+    </Card>
   );
 };
 
