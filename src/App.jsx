8e747d99fee266818400d69f7a449565d7a49a20
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-31 16:50:28 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,56 @@
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Header from './component/Header';
 import FeedBackList from './component/FeedBackList';
 import data from './data/FeedbackData';
 import './App.css';
 import FeedBackstat from './component/FeedBackstat';
 import FeedBackForm from './component/FeedBackForm';
+import About from './component/Pages/About';
 
 function App() {
   const [feedback, setFeedback] = useState(data);
 
+  //DeletefeedBack
   const DeletefeedBack = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
+  //AddfeedBack
   const AddFeedBAck = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
 
   return (
-    <>
-      <Header text={'Faizan App'} />
+    <Router>
+      <Link to={{ pathname: '/' }}>
+        <Header text={'Faizan App'} />
+      </Link>
       <div className="container">
-        <FeedBackForm addForm={AddFeedBAck} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <FeedBackForm addForm={AddFeedBAck} />
 
-        <FeedBackstat feedback={feedback} />
+                <FeedBackstat feedback={feedback} />
 
-        <FeedBackList feedback={feedback} handleDelete={DeletefeedBack} />
+                <FeedBackList
+                  feedback={feedback}
+                  handleDelete={DeletefeedBack}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/component/Pages/About.jsx
@@ -0,0 +1,15 @@
+import Card from '../Shared/Card';
+
+const About = () => {
+  return (
+    <Card className="about">
+      <h1> About this Project</h1>
+      <p>This project is React App to leave a feedback for a product </p>
+      <p>
+        <a href="/"> Back To Home</a>
+      </p>
+    </Card>
+  );
+};
+
+export default About;
